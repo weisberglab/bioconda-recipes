02dabacd33ba019b5f283aa0595557b9930619f5
Alexandra Weisberg <weisbeal@onid.oregonstate.edu>
2023-03-16 21:39:53 -0700

    added more options

--- recipes/beav/build.sh
+++ recipes/beav/build.sh
@@ -1,3 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo "test"
+mkdir -p $PREFIX/bin
+mkdir -p $PREFIX/scripts
+mkdir -p $PREFIX/databases
+mkdir -p $PREFIX/models
+mkdir -p $PREFIX/test_data
+
+cp beav $PREFIX/bin
+cp -r scripts/* $PREFIX/scripts
+cp -r databases/* $PREFIX/databases
+cp -r models/* $PREFIX/models
+cp -r test_data/* $PREFIX/test_data
+
